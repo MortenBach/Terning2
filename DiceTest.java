eca1175056f9113df73763e9a6ce5d0f1c0621cd
BrianAngelo <angeloandersen@hotmail.com>
2016-09-26 21:42:25 +0200

    Update DiceTest.java

--- DiceTest.java
+++ DiceTest.java
@@ -1,59 +1,67 @@
-package terning;
-
 import static org.junit.Assert.*;
-
-import java.util.Scanner;
 
 import org.junit.Test;
 
 public class DiceTest {
-
+	
+int numberOfRolls = 60000, result;
+	
 	@Test
-	public void test1() {
-		Dice a = new Dice();
-		int result = a.roll();
+	public void DiceIntervalTest() {
 		
-		assertTrue(result <=6 && result >=1);
+		Dice a = new Dice(); 
+		
+		for (int roll = 0; roll <= numberOfRolls;)
+		{
+			roll += 1;
+			result = a.roll();
+			
+			if ( result > 6 || result < 1)
+				break;
+			
+		}
+		assertTrue(result >= 1 && result <= 6);
 	}
 
+	int dice1, dice2, dice3, dice4, dice5, dice6, counter;
+	
 	@Test
-	public void test2() {
+	public void valueOccur() {
 				
 		Dice b = new Dice();
-			
-		int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0, counter, roller;
 		
-		int n = 60000;
-		
-		
-		
-		for (int i=1; i<n; i++)	{
+		for (int i = 1; i < numberOfRolls; i++)	{
 			
 			counter = b.roll();
 			
 			switch (counter){
 			case 1:
-				count1 = count1 + 1;
+				dice1 += 1;
 				break;
 			case 2:
-				count2 = count2 + 1;
+				dice2 += 1;
 				break;
 			case 3:
-				count3 = count3 + 1;
+				dice3 += 1;
 				break;
 			case 4:
-				count4 = count4 + 1;
+				dice4 += 1;
 				break;
 			case 5:
-				count5 = count5 + 1;
+				dice5 += 1;
 				break;
 			case 6:
-				count6 = count6 + 1;
+				dice6 += 1;
 				break;
 		}
+		}
+		System.out.print("Number of 1`s: " + dice1 + "\tNumber of 2`s: " + dice2 + "\tNumber of 3`s: " + dice3 + "\tNumber of 4`s: " + dice4 + "\tNumber of 5`s: " + dice5 + "\tNumber of 6`s: " + dice6);
 		
-		
-				
-		
-			
-}}}
+		assertTrue(dice1 >= (10000-400) && dice1 <= (10000+400));
+		assertTrue(dice2 >= (10000-400) && dice2 <= (10000+400));
+		assertTrue(dice3 >= (10000-400) && dice3 <= (10000+400));
+		assertTrue(dice4 >= (10000-400) && dice4 <= (10000+400));
+		assertTrue(dice5 >= (10000-400) && dice5 <= (10000+400));
+		assertTrue(dice6 >= (10000-400) && dice6 <= (10000+400));
+	}
+}
